773a25e6ae12e2b7d9b9c5d213455ea150a8f6b4
unknown <you@example.com>
2026-03-13 14:55:40 +0100

    Actualizar proyecto para producción, ajustes en Firebase y env

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,3 +1,4 @@
+// src/firebase/firebase.service.ts
 import { Injectable, OnModuleInit } from '@nestjs/common'
 import * as admin from 'firebase-admin'
 import * as path from 'path'
@@ -8,20 +9,19 @@ export class FirebaseService implements OnModuleInit {
   private firebaseApp: admin.app.App
 
   onModuleInit() {
-
-    const serviceAccount = require(
-      path.join(__dirname, '../../firebase/admin-firebase.json')
-    )
+    // Ruta absoluta hacia el JSON de producción dentro de dist/firebase
+    const serviceAccountPath = path.join(process.cwd(), 'dist/firebase/admin-firebase.json')
+    
+    const serviceAccount = require(serviceAccountPath)
 
     this.firebaseApp = admin.initializeApp({
       credential: admin.credential.cert(serviceAccount)
     })
 
-    console.log('🔥 Firebase initialized')
+    console.log('🔥 Firebase initialized (Production)')
   }
 
   getMessaging() {
     return this.firebaseApp.messaging()
   }
-
 }
