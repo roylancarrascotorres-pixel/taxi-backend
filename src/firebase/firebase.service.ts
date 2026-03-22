7d6f496a993144b85e500e6841d00972ddca900e
unknown <you@example.com>
2026-03-22 04:49:50 +0100

    Actualiza FirebaseService para usar variable de entorno y elimina JSON local

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,21 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as admin from 'firebase-admin';
-import * as path from 'path';
 
 @Injectable()
 export class FirebaseService implements OnModuleInit {
   private firebaseApp!: admin.app.App;
 
   onModuleInit() {
-    const serviceAccount = require(
-      path.join(__dirname, '../../firebase/avenzo-9701-firebase-adminsdk-fbsvc-0bd8c651cd.json')
-    );
+    // Tomamos la variable de entorno y la convertimos a objeto JSON
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
 
     this.firebaseApp = admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
 
-    console.log('🔥 Firebase Admin initialized for production');
+    console.log('🔥 Firebase Admin initialized with environment variable');
   }
 
   getMessaging() {
